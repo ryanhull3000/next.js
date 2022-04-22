9d3d3c3497eeffd65f68e6488fd748dcb5c84d97
Ryan Hull <mintdesign3000@gmail.com>
2022-04-22 13:42:39 -0600

    add .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd52dac9f102cbe7"; }
+/******/ 		__webpack_require__.h = function() { return "1fb45ab234102dc3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
